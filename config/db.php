c9364f73975a1cc1eed6c5e88fbd80dbf294c81f
ShaMan19 <valckov.vladimir@gmail.com>
2017-08-17 23:51:14 +0700

    first

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=treasure',
+    'username' => 'root',
+    'password' => '123qwe',
+    'charset' => 'utf8',
+];
---
+++ modules/admin/views/default/index.php
@@ -0,0 +1,6 @@
+<div class="admin-default-index">
+    <h1><?= $this->context->action->uniqueId ?></h1>
+    <p>
+        Добро пожаловать в админ панель
+    </p>
+</div>
